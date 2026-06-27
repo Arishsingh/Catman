c647d631932196dcf23ab0e3d2dd8afc2cc15fca
Arishsingh <arishsingh9469@gmail.com>
2026-06-28 03:22:29 +0530

    Auto-commit: update Catman (2026-06-28 03:22:29)

--- src/components/result-screen.tsx
+++ src/components/result-screen.tsx
@@ -327,8 +327,7 @@ export function ResultScreen({
           </p>
           <p>{lore.body}</p>
           <p className="text-white/50">{lore.close}</p>
-          <p className="pt-2 text-white/95">btw</p>
-          <p>{reading}</p>
+          <p className="pt-2">btw {reading}</p>
         </motion.div>
 
         <motion.div
